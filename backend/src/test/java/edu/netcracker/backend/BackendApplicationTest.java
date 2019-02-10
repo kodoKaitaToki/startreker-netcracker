ba0e3a192633835db9a9bffcec4c100354f055db
EugeneLokotariev <your_email@whatever.com>
2019-02-10 16:04:20 +0200

    travis.yml and pom.xml were changed in test purposes

--- backend/src/test/java/edu/netcracker/backend/BackendApplicationTest.java
+++ backend/src/test/java/edu/netcracker/backend/BackendApplicationTest.java
@@ -7,12 +7,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class BackendApplicationTests {
+public class BackendApplicationTest {
 
     @Test
     public void contextLoads() {
 
     }
-
 }
-
