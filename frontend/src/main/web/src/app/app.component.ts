b560fd394be99b97c6cc0434d963e52ccc535173
EugeneLokotariev <your_email@whatever.com>
2019-02-11 13:34:04 +0200

    Backend: User, UserController was added. Frontend: a http request to UserController was added

--- frontend/src/main/web/src/app/app.component.ts
+++ frontend/src/main/web/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'web';
+
+  user: any = {};
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit()  {
+
+    this.getRequest()
+      .subscribe(data => {
+        this.user = data;
+      })
+  }
+
+  getRequest() {
+
+    return this.http.get('http://localhost:8090/api/user');
+  }
 }
