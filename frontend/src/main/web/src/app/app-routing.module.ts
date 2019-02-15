d28c3a5adcce6ea57117578b79c201b22ff3f8cf
roman.bessmertnyi <roman.bezsmerniy@gmail.com>
2019-02-15 16:44:33 +0200

    Revert "Added admin navbar and approver crud page"
    This reverts commit dbbb080de03a94a958fc011ed9c70cf038bdcbad.

--- frontend/src/main/web/src/app/app-routing.module.ts
+++ frontend/src/main/web/src/app/app-routing.module.ts
@@ -1,15 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AdminComponent }      from './admin/admin.component';
-import { ApproverCrudComponent } from './admin/approver-crud/approver-crud.component';
 
-const routes: Routes = [
-                        { path: 'admin', component: AdminComponent,
-                          children: [
-                                { path: 'approvers', component: ApproverCrudComponent }
-                                ]
-                        }
-                       ];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
