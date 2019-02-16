a9ddf041636f5f42b23b45c192c6c921ab90ebf3
JulDan <danihatchjulia@gmail.com>
2019-02-16 18:15:58 +0200

    Folders structure was changed in frontend/src/main/web/src/app. New folder "pages" was created. Landing and not-found-page were moved to "pages" folder. All the components of landing page (carousel, features and so on) were moved to "/pages/landing". Test component was deleted as far as we don't need to make tests for frontend

--- frontend/src/main/web/src/app/app-routing.module.ts
+++ frontend/src/main/web/src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { LandingComponent } from './landing/landing.component';
-import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
+import { LandingComponent } from './pages/landing/landing.component';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 
 const routes: Routes = [
   { path: '', component: LandingComponent},
