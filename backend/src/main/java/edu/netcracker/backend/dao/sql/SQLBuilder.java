b4a53082511f9c58d7455e272362651135d89f3d
sottiletta <lirrow@gmail.com>
2019-02-17 20:03:34 +0300

    Service, trip and trouble ticket statistics. CrudDAO support for LocalDate and LocalDateTime.

---
+++ backend/src/main/java/edu/netcracker/backend/dao/sql/SQLBuilder.java
@@ -0,0 +1,10 @@
+package edu.netcracker.backend.dao.sql;
+
+public interface SQLBuilder {
+
+    String assembleInsertSql();
+    String assembleUpdateSql();
+    String assembleDeleteSql();
+    String assembleExistsSql();
+    String assembleSelectSql();
+}
---
+++ backend/src/main/java/edu/netcracker/backend/util/ReportStatus.java
@@ -0,0 +1,20 @@
+package edu.netcracker.backend.util;
+
+import lombok.Getter;
+
+@Getter
+public enum ReportStatus {
+
+    OPEN(1),
+    IN_PROGRESS(2),
+    ANSWERED(3),
+    REOPENED(4),
+    RATED(5),
+    REMOVED(6);
+
+    private final int databaseValue;
+
+    ReportStatus(int databaseValue){
+        this.databaseValue = databaseValue;
+    }
+}
