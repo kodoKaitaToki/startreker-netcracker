41567417aea3eecf78458cbd9b47110c58535d4d
roman.bessmertnyi <roman.bezsmerniy@gmail.com>
2019-02-20 20:09:21 +0200

    added graph

--- frontend/src/main/web/src/app/pages/admin/trouble-statistics/trouble-statistics.component.ts
+++ frontend/src/main/web/src/app/pages/admin/trouble-statistics/trouble-statistics.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as CanvasJS from '../canvasjs.min';
 
 @Component({
   selector: 'app-trouble-statistics',
@@ -10,6 +11,30 @@ export class TroubleStatisticsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    let ticketChart = new CanvasJS.Chart("ticketChartContainer", {
+            animationEnabled: true,
+            exportEnabled: true,
+            title: {
+              text: "Trouble ticket categories:"
+            },
+            data: [{
+              type: "doughnut",
+              startAngle: 60,
+              indexLabelFontSize: 17,
+              indexLabel: "{label} - #percent%",
+              toolTipContent: "<b>{label}:</b> {y} (#percent%)",
+              dataPoints: [
+                { y: 1, label: "Reopened" },
+                { y: 1, label: "Opened" },
+                { y: 1, label: "Rated" },
+                { y: 1, label: "Answered"},
+                { y: 1, label: "In progress"},
+              ]
+            }]
+          });
+
+    ticketChart.render();
+
   }
 
 }
