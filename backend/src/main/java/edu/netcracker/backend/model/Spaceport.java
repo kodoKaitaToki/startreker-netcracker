5d97eae85a08a61905f0491851b41d1e29f5ac7e
ilyinalex <ilinoleksiii@gmail.com>
2019-02-22 00:07:50 +0200

    K19DKPA-62 models: Planet, Spaceport

---
+++ backend/src/main/java/edu/netcracker/backend/model/Spaceport.java
@@ -0,0 +1,29 @@
+package edu.netcracker.backend.model;
+
+import edu.netcracker.backend.dao.annotations.Attribute;
+import edu.netcracker.backend.dao.annotations.PrimaryKey;
+import edu.netcracker.backend.dao.annotations.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Table("spaceport")
+public class Spaceport {
+    @PrimaryKey("spaceport_id")
+    @EqualsAndHashCode.Include
+    private Long spaceportId;
+
+    @Attribute("spaceport-name")
+    private String spaceportName;
+
+    @Attribute("creation_date")
+    private LocalDate creationDate;
+
+    @Attribute("planet_id")
+    private Long planetId;
+}
