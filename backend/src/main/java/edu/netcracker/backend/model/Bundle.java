e88e0a95026f40f370f1476fc12df8cce85d412a
Oleh <olehi.demydenko@gmail.com>
2019-02-23 17:08:58 +0200

    Created models for Bundles, Services and PossibleServices.

---
+++ backend/src/main/java/edu/netcracker/backend/model/Bundle.java
@@ -0,0 +1,42 @@
+package edu.netcracker.backend.model;
+
+import edu.netcracker.backend.dao.annotations.Attribute;
+import edu.netcracker.backend.dao.annotations.PrimaryKey;
+import edu.netcracker.backend.dao.annotations.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Table("bundle")
+public class Bundle {
+
+    @PrimaryKey("bundle_id")
+    @EqualsAndHashCode.Include
+    private Integer bundleId;
+
+    @Attribute("start_date")
+    private LocalDate startDate;
+
+    @Attribute("finish_date")
+    private LocalDate finishDate;
+
+    @Attribute("bundle_price")
+    private Integer bundlePrice;
+
+    @Attribute("start_date")
+    private String bundleDescription;
+
+    @Attribute("start_date")
+    private String bundlePhotoUri;
+
+    private List<TicketClass> bundle_tickets;
+
+    private List<Service> bundle_services;
+
+}
---
+++ backend/src/main/java/edu/netcracker/backend/model/Service.java
@@ -0,0 +1,27 @@
+package edu.netcracker.backend.model;
+
+import edu.netcracker.backend.dao.annotations.Attribute;
+import edu.netcracker.backend.dao.annotations.PrimaryKey;
+import edu.netcracker.backend.dao.annotations.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Table("service")
+public class Service {
+
+    @PrimaryKey("service_id")
+    @EqualsAndHashCode.Include
+    private Integer serviceId;
+
+    @Attribute("service_name")
+    private String serviceName;
+
+    @Attribute("service_description")
+    private String serviceDescription;
+
+
+}
