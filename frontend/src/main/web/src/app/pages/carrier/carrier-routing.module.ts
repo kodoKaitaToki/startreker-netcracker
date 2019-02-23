3b2075e35fa70067ca49acb637837c73f5085be6
roman.bessmertnyi <roman.bezsmerniy@gmail.com>
2019-02-23 13:47:42 +0200

    Added modules, pages and routing

---
+++ frontend/src/main/web/src/app/pages/carrier/carrier-routing.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { IndexComponent } from './index/index.component';
+import { TripsComponent } from './trips/trips.component';
+import { ServicesComponent } from './services/services.component';
+import { SuggestionsComponent } from './suggestions/suggestions.component';
+import { DiscountsComponent } from './discounts/discounts.component';
+import { DashboardsComponent } from './dashboard/dashboards/dashboards.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: IndexComponent
+    },
+    {
+        path: 'trips',
+        component: TripsComponent
+    },
+    {
+        path: 'services',
+        component: ServicesComponent
+    },
+    {
+        path: 'suggestions',
+        component: SuggestionsComponent
+    },
+    {
+        path: 'discounts',
+        component: DiscountsComponent
+    },
+    {
+        path: 'dashboards',
+        component: DashboardsComponent
+    },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class CarrierRoutingModule { }
---
+++ frontend/src/main/web/src/app/pages/carrier/carrier.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CarrierRoutingModule } from './carrier-routing.module';
+import { NavbarComponent } from './navbar/navbar.component';
+import { IndexComponent } from './index/index.component';
+import { TripsComponent } from './trips/trips.component';
+import { ServicesComponent } from './services/services.component';
+import { SuggestionsComponent } from './suggestions/suggestions.component';
+import { DiscountsComponent } from './discounts/discounts.component';
+import { DashboardsComponent } from './dashboard/dashboards/dashboards.component';
+
+@NgModule({
+  declarations: [NavbarComponent, IndexComponent, TripsComponent, ServicesComponent, SuggestionsComponent, DiscountsComponent, DashboardsComponent],
+  imports: [
+    CommonModule,
+    CarrierRoutingModule
+  ]
+})
+export class CarrierModule { }
