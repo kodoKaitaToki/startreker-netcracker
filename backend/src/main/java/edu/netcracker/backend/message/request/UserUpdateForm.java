1954561b99ef888a7c5f41cc7c9aad76dd2f05c7
Oleh <olehi.demydenko@gmail.com>
2019-02-23 21:45:03 +0200

    Remove @JsonProperty from id in UserUpdateForm.

--- backend/src/main/java/edu/netcracker/backend/message/request/UserUpdateForm.java
+++ backend/src/main/java/edu/netcracker/backend/message/request/UserUpdateForm.java
@@ -1,7 +1,6 @@
 package edu.netcracker.backend.message.request;
 
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,6 +15,5 @@ import javax.validation.constraints.NotNull;
 public class UserUpdateForm extends UserForm {
 
     @NotNull
-    @JsonProperty("user_id")
     private Integer userId;
 }
