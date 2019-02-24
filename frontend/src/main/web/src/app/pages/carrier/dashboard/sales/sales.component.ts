64142d1b0115ef837078423bd31f53aacb139638
roman.bessmertnyi <roman.bezsmerniy@gmail.com>
2019-02-24 11:33:39 +0200

    Created basic markup

--- frontend/src/main/web/src/app/pages/carrier/dashboard/sales/sales.component.ts
+++ frontend/src/main/web/src/app/pages/carrier/dashboard/sales/sales.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as CanvasJS from '../../canvasjs.min';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-sales',
@@ -6,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sales.component.scss']
 })
 export class SalesComponent implements OnInit {
+
+  form = new FormGroup({
+    dataPeriod: new FormControl('perWeek'),
+    fromDate: new FormControl(new Date()),
+    toDate: new FormControl(new Date()),
+  });
 
   constructor() { }
 
