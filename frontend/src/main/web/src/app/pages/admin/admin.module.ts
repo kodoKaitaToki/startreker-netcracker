03f682be2da8736474e11da1c9089ea5ab81f8b3
roman.bessmertnyi <roman.bezsmerniy@gmail.com>
2019-03-08 16:01:12 +0200

    Fixed module deletions

--- frontend/src/main/web/src/app/pages/admin/admin.module.ts
+++ frontend/src/main/web/src/app/pages/admin/admin.module.ts
@@ -7,26 +7,37 @@ import {IndexComponent} from './index/index.component';
 import {ApproverCrudComponent} from './approver/approver-crud/approver-crud.component';
 import {CarrierCrudComponent} from './carrier/carrier-crud/carrier-crud.component';
 import {DashboardsComponent} from './dashboards/dashboards.component';
-import {BundlesComponentComponent} from './bundles/bundles-component/bundles-component.component';
-import {BundlesTableComponent} from './bundles/bundles-table/bundles-table.component';
-import {BundlesCrudComponent} from './bundles/bundles-crud/bundles-crud.component';
+
 import {ApproverComponentComponent} from './approver/approver-component/approver-component.component';
 import {ApproverTableComponent} from './approver/approver-table/approver-table.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {FilterPipePipe} from './carrier/filter-pipe.pipe';
 import {AprroverShowStatusPipe} from './approver/shared/pipes/aprrover-show-status.pipe';
+
+import {BundlesComponentComponent} from './bundles/bundles-component/bundles-component.component';
+import {BundlesTableComponent} from './bundles/bundles-table/bundles-table.component';
+import {BundlesCrudComponent} from './bundles/bundles-crud/bundles-crud.component';
+
+import {TripsServicesComponent} from './dashboards/trips-services/trips-services.component';
+import {TripDashboardComponent} from './dashboards/trips-services/trip-dashboard/trip-dashboard.component';
+import {ServiceDashboardComponent} from './dashboards/trips-services/service-dashboard/service-dashboard.component';
+
 import {DashboardDeltaComponent} from './dashboards/dashboard-delta/dashboard-delta.component';
 import {TroubleStatisticsComponent} from './dashboards/trouble-statistics/trouble-statistics.component';
+
 import {CarrierShowStatusPipe} from './carrier/carrier-show-status.pipe';
+
+import {CostsComponent} from './dashboards/costs/costs.component';
 import {CommonChartComponent} from './dashboards/costs/common-chart/common-chart.component';
 import {CarCostDashComponent} from './dashboards/costs/car-cost-dash/car-cost-dash.component';
+
 import {CarrierComponentComponent} from './carrier/carrier-component/carrier-component.component';
 import {CarrierTableComponent} from './carrier/carrier-table/carrier-table.component';
 import {NgxPaginationModule} from 'ngx-pagination';
 
 import {TroubleStatisticsService} from './dashboards/trouble-statistics.service';
 import {ApproverService} from "./approver/shared/service/approver.service";
-import {ApproverFilterPipe} from "./approver/shared/pipes/approver-filter.pipe";
+import {ApproverFilterPipe} from './approver/shared/pipes/approver-filter.pipe';
 
 
 @NgModule({
@@ -38,6 +49,9 @@ import {ApproverFilterPipe} from "./approver/shared/pipes/approver-filter.pipe";
               BundlesCrudComponent,
               BundlesComponentComponent,
               BundlesTableComponent,
+              TripsServicesComponent,
+              TripDashboardComponent,
+              ServiceDashboardComponent,
               DashboardsComponent,
               ApproverComponentComponent,
               ApproverTableComponent,
@@ -49,6 +63,7 @@ import {ApproverFilterPipe} from "./approver/shared/pipes/approver-filter.pipe";
               CarrierComponentComponent,
               CarrierTableComponent,
               CarrierShowStatusPipe,
+              CostsComponent,
               CommonChartComponent,
               CarCostDashComponent,
               ApproverFilterPipe
