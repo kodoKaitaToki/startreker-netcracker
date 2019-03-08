1ea8120aa94dfc6aaf6d733d6f4a979590360f48
roman.bessmertnyi <roman.bezsmerniy@gmail.com>
2019-03-08 15:52:44 +0200

    dashboard fix

--- frontend/src/main/web/src/app/pages/admin/admin-routing.module.ts
+++ frontend/src/main/web/src/app/pages/admin/admin-routing.module.ts
@@ -3,8 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { IndexComponent } from './index/index.component';
 import { CarrierCrudComponent } from './carrier/carrier-crud/carrier-crud.component';
 import { BundlesCrudComponent } from './bundles/bundles-crud/bundles-crud.component';
+
+import { TripsServicesComponent } from './dashboards/trips-services/trips-services.component';
 import { DashboardsComponent } from './dashboards/dashboards.component';
 import { ApproverCrudComponent } from './approver/approver-crud/approver-crud.component';
+import { CostsComponent } from './dashboards/costs/costs.component';
+import { DashboardDeltaComponent } from './dashboards/dashboard-delta/dashboard-delta.component';
+import { TroubleStatisticsComponent } from './dashboards/trouble-statistics/trouble-statistics.component';
 
 const routes: Routes = [
   {
@@ -25,7 +30,13 @@ const routes: Routes = [
   },
   {
       path: 'dashboards',
-      component: DashboardsComponent
+      component: DashboardsComponent,
+      children: [
+              { path: 'costs', component: CostsComponent },
+              { path: 'popularity', component: DashboardDeltaComponent},
+              { path: 'trouble-tickets', component: TroubleStatisticsComponent},
+              { path: 'trips-services-distribution', component: TripsServicesComponent}
+          ]
   },
 ];
 
