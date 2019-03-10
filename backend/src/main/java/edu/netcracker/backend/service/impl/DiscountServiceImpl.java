f5c7383093bc2d1c98d596ae19c5b18318ae512e
Viktor Ivanov <ivanov.viktorgennadiy@gmail.com>
2019-03-10 15:22:09 +0200

    Add logging in Discount service

--- backend/src/main/java/edu/netcracker/backend/service/impl/DiscountServiceImpl.java
+++ backend/src/main/java/edu/netcracker/backend/service/impl/DiscountServiceImpl.java
@@ -5,6 +5,8 @@ import edu.netcracker.backend.dao.DiscountDAO;
 import edu.netcracker.backend.message.request.DiscountDTO;
 import edu.netcracker.backend.model.Discount;
 import edu.netcracker.backend.service.DiscountService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -17,6 +19,8 @@ import java.util.stream.Collectors;
 @Service
 public class DiscountServiceImpl implements DiscountService {
 
+    private static final Logger logger = LoggerFactory.getLogger(DiscountServiceImpl.class);
+
     private static final String DATE_PATTERN = "dd-MM-yyyy";
 
     private final DiscountDAO discountDAO;
@@ -28,6 +32,8 @@ public class DiscountServiceImpl implements DiscountService {
 
     @Override
     public List<DiscountDTO> getDiscountDTOs(List<Long> entityIds) {
+        logger.debug("getting discounts that belong to entities with id " + entityIds);
+
         List<Discount> discounts = discountDAO.findIn(entityIds);
         checkingDiscountsForOverdue(discounts);
         return discounts.stream().map(discount -> DiscountDTO.toDiscountDTO(discount, DATE_PATTERN))
@@ -36,7 +42,9 @@ public class DiscountServiceImpl implements DiscountService {
 
     @Override
     public DiscountDTO saveDiscount(DiscountDTO discountDTO) {
+        logger.debug("save discount");
         if (discountDTO == null) {
+            logger.error("given discount is null");
             throw new RequestException("Discount is null", HttpStatus.BAD_REQUEST);
         }
 
@@ -48,9 +56,11 @@ public class DiscountServiceImpl implements DiscountService {
 
     @Override
     public DiscountDTO deleteDiscount(Number discountId) {
+        logger.debug("delete discount with id " + discountId);
         Optional<Discount> optionalDiscount = discountDAO.find(discountId);
 
         if (!optionalDiscount.isPresent()) {
+            logger.error("discount with id " + discountId + " not found");
             throw new RequestException("No such discount",
                     HttpStatus.NOT_FOUND);
         }
@@ -95,19 +105,23 @@ public class DiscountServiceImpl implements DiscountService {
         Discount discount = Discount.toDiscount(discountDTO);
 
         if (discount.getFinishDate().isBefore(LocalDateTime.now())) {
+            logger.error("Finish date of discount is less than current date");
             throw new RequestException("Finish date of discount is less than current date", HttpStatus.BAD_REQUEST);
         }
 
         if (discount.getFinishDate().isBefore(discount.getStartDate())) {
+            logger.error("Finish date of discount is less than start date of discount");
             throw new RequestException("Finish date of discount is less than start date of discount",
                     HttpStatus.BAD_REQUEST);
         }
 
         if (discount.getDiscountRate() <= 0) {
+            logger.error("Discount rate must be greater than 0. Found " + discount.getDiscountRate());
             throw new RequestException("Discount rate must be greater than 0", HttpStatus.BAD_REQUEST);
         }
 
         if (discount.getIsPercent() && discount.getDiscountRate() > 100) {
+            logger.error("Discount rate in percents must be less than 100. Found " +  discount.getDiscountRate());
             throw new RequestException("Discount rate in percents must be less than 100", HttpStatus.BAD_REQUEST);
         }
 
