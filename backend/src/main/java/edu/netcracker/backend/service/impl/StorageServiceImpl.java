bf8c364988566cfc3640759ec7f0dbdd2a54e994
roman.bessmertnyi <roman.bezsmerniy@gmail.com>
2019-03-10 09:21:04 +0200

    startup bugfix

--- backend/src/main/java/edu/netcracker/backend/service/impl/StorageServiceImpl.java
+++ backend/src/main/java/edu/netcracker/backend/service/impl/StorageServiceImpl.java
@@ -47,10 +47,13 @@ public class StorageServiceImpl implements edu.netcracker.backend.service.Storag
     }
 
     public void init() {
-        try {
-            Files.createDirectory(rootLocation);
-        } catch (IOException e) {
-            throw new RuntimeException("Could not initialize storage!");
+        if (!rootLocation.toFile().exists())
+        {
+            try {
+                Files.createDirectory(rootLocation);
+            } catch (IOException e) {
+                throw new RuntimeException("Could not initialize storage!");
+            }
         }
     }
 }
