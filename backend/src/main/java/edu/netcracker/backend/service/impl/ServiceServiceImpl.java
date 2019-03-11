e20770f551162fda3b7008773ff612005733f166
roman.bessmertnyi <roman.bezsmerniy@gmail.com>
2019-03-11 07:54:18 +0200

    injection fix

--- backend/src/main/java/edu/netcracker/backend/service/impl/ServiceServiceImpl.java
+++ backend/src/main/java/edu/netcracker/backend/service/impl/ServiceServiceImpl.java
@@ -1,7 +1,6 @@
 package edu.netcracker.backend.service.impl;
 
 import edu.netcracker.backend.controller.exception.RequestException;
-import edu.netcracker.backend.dao.ApproverDAO;
 import edu.netcracker.backend.dao.ServiceDAO;
 import edu.netcracker.backend.dao.ServiceReplyDAO;
 import edu.netcracker.backend.message.request.ServiceCreateForm;
@@ -25,19 +24,19 @@ import java.util.Objects;
 @Service
 public class ServiceServiceImpl implements ServiceService {
 
-    @Autowired
     private ServiceDAO serviceDAO;
 
-    @Autowired
     private ServiceReplyDAO serviceReplyDAO;
 
-    @Autowired
     private UserService userService;
 
     private Integer carrierId = 7;
 
-    public ServiceServiceImpl(){
-        //setCurCarrier();
+    @Autowired
+    public ServiceServiceImpl(ServiceDAO serviceDAO, ServiceReplyDAO serviceReplyDAO, UserService userService){
+        this.serviceDAO = serviceDAO;
+        this.serviceReplyDAO = serviceReplyDAO;
+        this.userService = userService;
     }
 
     @Override
