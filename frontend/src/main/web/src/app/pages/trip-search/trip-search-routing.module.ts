47c4d6575ea7b8b20751f230a73f8e533e42ab52
ilyinalex <ilinoleksiii@gmail.com>
2019-03-14 16:03:49 +0200

    K19DKPA-224 add search-result

--- frontend/src/main/web/src/app/pages/trip-search/trip-search-routing.module.ts
+++ frontend/src/main/web/src/app/pages/trip-search/trip-search-routing.module.ts
@@ -1,11 +1,18 @@
 import {RouterModule, Routes} from "@angular/router";
 import {TripSearchComponent} from "./trip-search/trip-search.component";
 import {NgModule} from "@angular/core";
+import {SearchResultComponent} from "./search-result/search-result.component";
 
 const routes: Routes = [
   {
     path: '',
-    component: TripSearchComponent
+    component: TripSearchComponent,
+    children: [
+      {
+        path: '',
+        component: SearchResultComponent
+      }
+    ]
   }
 ];
 
