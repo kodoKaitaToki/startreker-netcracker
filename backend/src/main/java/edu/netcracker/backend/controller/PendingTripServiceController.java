73b8105e9fac978a97d323bd8e8f7681adc977dd
EugeneLokotariev <your_email@whatever.com>
2019-03-15 19:11:57 +0200

    K19DKPA-136 PendingTripServiceController - error handling was added

--- backend/src/main/java/edu/netcracker/backend/controller/PendingTripServiceController.java
+++ backend/src/main/java/edu/netcracker/backend/controller/PendingTripServiceController.java
@@ -1,10 +1,12 @@
 package edu.netcracker.backend.controller;
 
+import edu.netcracker.backend.controller.exception.RequestException;
 import edu.netcracker.backend.message.request.PendingActivationService;
 import edu.netcracker.backend.message.request.PendingActivationTrip;
 import edu.netcracker.backend.service.IPendingSrvc;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -42,7 +44,13 @@ public class PendingTripServiceController {
 
         log.debug("PendingTripServiceController.getServicesLimitOffset(Number limit, Number offset) was invoked with parameters limit = {}, offset = {}", limit, offset);
 
-        return pendingServicesSrvc.getPendingWithOffsetAndLimit(limit, offset);
+        if ((0 > limit.intValue()) || (0 > offset.intValue())) {
+            log.debug("PendingTripServiceController.getServicesLimitOffset(Number limit, Number offset) - request arguments are wrong. Bad request will be sent to the user");
+            throw new RequestException("Limit or offset is less than zero", HttpStatus.BAD_REQUEST);
+        } else {
+            log.debug("PendingTripServiceController.getServicesLimitOffset(Number limit, Number offset) was invoked with parameters limit = {}, offset = {} - service invoking", limit, offset);
+            return pendingServicesSrvc.getPendingWithOffsetAndLimit(limit, offset);
+        }
     }
 
     @RequestMapping(value = "/api/v1/approver/all-pending-trips-paging", method = RequestMethod.GET)
@@ -50,7 +58,13 @@ public class PendingTripServiceController {
 
         log.debug("PendingTripServiceController.getTripsLimitOffset(Number limit, Number offset) was invoked with parameters limit = {}, offset = {}", limit, offset);
 
-        return pendingTripsSrvc.getPendingWithOffsetAndLimit(limit, offset);
+        if ((0 > limit.intValue()) || (0 > offset.intValue())) {
+            log.debug("PendingTripServiceController.getTripsLimitOffset(Number limit, Number offset) - request arguments are wrong. Bad request will be sent to the user");
+            throw new RequestException("Limit or offset is less than zero", HttpStatus.BAD_REQUEST);
+        } else {
+            log.debug("PendingTripServiceController.getTripsLimitOffset(Number limit, Number offset) was invoked with parameters limit = {}, offset = {} - service invoking", limit, offset);
+            return pendingTripsSrvc.getPendingWithOffsetAndLimit(limit, offset);
+        }
     }
 
     @Autowired
