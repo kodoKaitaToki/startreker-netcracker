615e44ac3744a9b667c66ae0373f952d4bff2611
roman.bessmertnyi <roman.bezsmerniy@gmail.com>
2019-03-18 17:08:34 +0200

    Merge cleanup

--- frontend/src/main/web/src/app/pages/carrier/trips/shared/model/trip.ts
+++ frontend/src/main/web/src/app/pages/carrier/trips/shared/model/trip.ts
@@ -1,10 +1,7 @@
-import { TicketClass } from 'src/app/pages/admin/bundles/shared/model/ticket-class';
-
 export class Trip {
     trip_id: number;
     trip_status: string;
     departure_planet: string;
-    ticket_classes : TicketClass[];
     arrival_planet: string;
     departure_spaceport: string;
     arrival_spaceport: string;
