557c80439536d05bcd6f7908c09afa6055bde0ee
AlexXDD <OlehI.Demydenko@gmail.com>
2019-03-19 02:23:07 +0200

    K19DKPA-257: WIP written some stubs for demo

--- frontend/src/main/web/src/app/pages/flights/flights.component.ts
+++ frontend/src/main/web/src/app/pages/flights/flights.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Trip} from "./shared/models/trip.model";
 import {SearchService} from "./shared/services/search.service";
 import {FormControl, FormGroup} from "@angular/forms";
+import {FlightClass} from "./shared/models/flight-class.model";
 
 @Component({
   selector: 'app-flights',
@@ -15,7 +16,25 @@ export class FlightsComponent implements OnInit {
   searchForm: FormGroup;
 
   constructor(private svc: SearchService) {
-    this.getTripsByCriteria()
+    //this.getTripsByCriteria()
+
+    let trip: Trip = new Trip();
+    trip.departure_date = "20-02-2020 12:20:00";
+    trip.departure_spaceport_name = "SpaceX";
+    trip.departure_planet_name = "EARTH";
+    trip.arrival_date = "28-02-2020 22:28:00"
+    trip.arrival_spaceport_name = "Blackhole";
+    trip.arrival_planet_name = "MOON";
+    trip.ticket_classes = [];
+
+    let ticket_class = new FlightClass();
+    ticket_class.class_name = "Chad";
+    ticket_class.remaining_seats = 10;
+    ticket_class.ticket_price = 2000;
+    trip.ticket_classes.push(ticket_class);
+
+    this.trips.push(trip);
+    this.trips.push(trip);
   }
 
   ngOnInit() {
