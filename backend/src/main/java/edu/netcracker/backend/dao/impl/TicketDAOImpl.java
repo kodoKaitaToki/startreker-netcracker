253f6b652397205d7d17a8f2c65019de4e455db3
JulDan <danihatchjulia@gmail.com>
2019-03-23 11:26:29 +0200

    ticket DAO queries moved to .properties files some loggers added

--- backend/src/main/java/edu/netcracker/backend/dao/impl/TicketDAOImpl.java
+++ backend/src/main/java/edu/netcracker/backend/dao/impl/TicketDAOImpl.java
@@ -2,9 +2,9 @@ package edu.netcracker.backend.dao.impl;
 
 import edu.netcracker.backend.dao.TicketDAO;
 import edu.netcracker.backend.dao.mapper.history.HistoryTicketMapper;
-import edu.netcracker.backend.model.history.HistoryTicket;
 import edu.netcracker.backend.model.Ticket;
 import edu.netcracker.backend.model.User;
+import edu.netcracker.backend.model.history.HistoryTicket;
 import lombok.extern.log4j.Log4j2;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,10 +16,7 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
-import java.sql.Timestamp;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -37,36 +34,19 @@ public class TicketDAOImpl extends CrudDAOImpl<Ticket> implements TicketDAO {
     @Value("${FIND_ALL_BY_CLASS}")
     private String FIND_ALL_BY_CLASS;
 
-    private final String FIND_REMAINING_SEATS = "SELECT COUNT(ticket_id) "
-                                                + "FROM ticket "
-                                                + "WHERE class_id = ? AND passenger_id IS NULL";
+    @Value("${FIND_REMAINING_SEATS}")
+    private String FIND_REMAINING_SEATS;
 
+    @Value("${CREATE_EMPTY_TICKET_FOR_TICKET_CLASS}")
+    private String CREATE_EMPTY_TICKET_FOR_TICKET_CLASS;
 
-    private final String CREATE_EMPTY_TICKET_FOR_TICKET_CLASS = "INSERT INTO ticket (class_id, seat) VALUES (?, ?)";
+    @Value("${DELETE_ALL_TICKETS_OF_TICKET_CLASS}")
+    private String DELETE_ALL_TICKETS_OF_TICKET_CLASS;
 
-    private final String DELETE_ALL_TICKETS_OF_TICKET_CLASS = "DELETE FROM ticket WHERE class_id = ?";
+    @Value("${FIND_ALL_BY_USER}")
+    private static String FIND_ALL_BY_USER;
 
     private static final Logger logger = LoggerFactory.getLogger(TicketDAOImpl.class);
-
-    private final static String FIND_ALL_BY_USER = "SELECT t.ticket_id, t.seat, t.end_price, t.purchase_date, "
-                                                   + "tc.class_name, tr.departure_date, tr.arrival_date, "
-                                                   + "sd.spaceport_name, sa.spaceport_name, pa.planet_name, "
-                                                   + "pd.planet_name, u.user_name, t.b_bundle "
-                                                   + "FROM ticket t "
-                                                   + "JOIN ticket_class tc ON tc.class_id = t.class_id "
-                                                   + "JOIN trip tr ON tr.trip_id = tc.trip_id "
-                                                   + "JOIN spaceport sa ON sa.spaceport_id = tr.arrival_id "
-                                                   + "JOIN spaceport sd ON sd.spaceport_id = tr.departure_id "
-                                                   + "JOIN planet pa ON sa.planet_id = pa.planet_id "
-                                                   + "JOIN planet pd ON sd.planet_id = pd.planet_id "
-                                                   + "JOIN user_a u ON tr.carrier_id = u.user_id "
-                                                   + "WHERE t.passenger_id = :id "
-                                                   + "AND (t.purchase_date > :start_date OR CAST(:start_date AS TIMESTAMP) IS NULL) "
-                                                   + "AND (t.purchase_date < :end_date OR CAST(:end_date AS TIMESTAMP) IS NULL) "
-                                                   + "ORDER BY t.purchase_date DESC "
-                                                   + "LIMIT :limit "
-                                                   + "OFFSET :offset";
-
 
     public List<Ticket> findAllByClass(Number id) {
         ArrayList<Ticket> tickets = new ArrayList<>();
@@ -75,7 +55,7 @@ public class TicketDAOImpl extends CrudDAOImpl<Ticket> implements TicketDAO {
             tickets.addAll(getJdbcTemplate().query(FIND_ALL_BY_CLASS, new Object[]{id}, getGenericMapper()));
 
         } catch (EmptyResultDataAccessException e) {
-            log.error(e.getMessage());
+            logger.error(e.getMessage());
         }
 
         return tickets;
@@ -88,6 +68,7 @@ public class TicketDAOImpl extends CrudDAOImpl<Ticket> implements TicketDAO {
      */
     @Override
     public void deleteAllTicketsOfTicketClass(Long id) {
+        logger.debug("Deleting all tickets of ticket class with id {}", id);
         getJdbcTemplate().update(DELETE_ALL_TICKETS_OF_TICKET_CLASS, id);
     }
 
