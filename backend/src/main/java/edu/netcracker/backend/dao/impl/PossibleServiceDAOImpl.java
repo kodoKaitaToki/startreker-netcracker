0b8bd1874a572c1b6508358591f67976140e3b1a
ilyinalex <ilinoleksiii@gmail.com>
2019-03-25 19:27:54 +0200

    K19DKPA-287 add some logs, move sql to properties, minor changes/

--- backend/src/main/java/edu/netcracker/backend/dao/impl/PossibleServiceDAOImpl.java
+++ backend/src/main/java/edu/netcracker/backend/dao/impl/PossibleServiceDAOImpl.java
@@ -6,37 +6,31 @@ import edu.netcracker.backend.model.PossibleService;
 import edu.netcracker.backend.model.Service;
 import edu.netcracker.backend.model.ServiceDescr;
 import edu.netcracker.backend.model.Ticket;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j(topic = "log")
 @Repository
+@PropertySource("classpath:sql/possibleservicedao.properties")
 public class PossibleServiceDAOImpl extends CrudDAOImpl<PossibleService> implements PossibleServiceDAO {
 
     private ServiceDAO serviceDAO;
 
-    private String FIND_ALL_WITH_CLASS_ID = "SELECT p_service_id, "
-                                            + "service_id, "
-                                            + "class_id, "
-                                            + "service_price, "
-                                            + "p_service_status "
-                                            + "FROM possible_service WHERE class_id = ? AND p_service_status = 1";
+    @Value("${FIND_ALL_WITH_CLASS_ID}")
+    private String FIND_ALL_WITH_CLASS_ID;
 
-    private String FIND_ALL_P_SERVICES_BY_SUGGESTION_ID = "SELECT possible_service.p_service_id, "
-                                                          + "service_id, "
-                                                          + "class_id, "
-                                                          + "service_price, "
-                                                          + "p_service_status FROM possible_service "
-                                                          + "INNER JOIN suggested_service ON possible_service.p_service_id = suggested_service.p_service_id "
-                                                          + "WHERE suggestion_id = ? AND p_service_status = 1";
+    @Value("${FIND_ALL_P_SERVICES_BY_SUGGESTION_ID}")
+    private String FIND_ALL_P_SERVICES_BY_SUGGESTION_ID;
 
-    private static final String BUY_P_SERVICE = "INSERT INTO bought_service ("
-                                                + "p_service_id, "
-                                                + "ticket_id) "
-                                                + "VALUES (?, ?)";
+    @Value("${BUY_P_SERVICE}")
+    private String BUY_P_SERVICE;
 
     @Autowired
     public PossibleServiceDAOImpl(ServiceDAO serviceDAO) {
@@ -45,9 +39,12 @@ public class PossibleServiceDAOImpl extends CrudDAOImpl<PossibleService> impleme
 
     @Override
     public Optional<PossibleService> find(Number id) {
+        log.debug("Getting possibleService by id {}", id);
+
         Optional<PossibleService> optPossibleService = super.find(id);
 
         if (!optPossibleService.isPresent()) {
+            log.error("Possible service with id {} not found", id);
             return Optional.empty();
         }
 
@@ -61,12 +58,15 @@ public class PossibleServiceDAOImpl extends CrudDAOImpl<PossibleService> impleme
 
     @Override
     public void delete(PossibleService possibleService) {
+        log.debug("Deleting possibleService with id {}", possibleService.getPServiceId());
         possibleService.setPServiceStatus(2L);
         update(possibleService);
     }
 
     @Override
     public List<PossibleService> findAllWithClassId(Number id) {
+        log.debug("Getting all possible services with id {}", id);
+
         List<PossibleService> possibleServices = new ArrayList<>();
 
         possibleServices.addAll(getJdbcTemplate().query(FIND_ALL_WITH_CLASS_ID, new Object[]{id}, getGenericMapper()));
@@ -77,11 +77,15 @@ public class PossibleServiceDAOImpl extends CrudDAOImpl<PossibleService> impleme
     }
 
     private Optional<ServiceDescr> findService(PossibleService possibleService) {
+        log.debug("Getting service by possible service with id {}", possibleService.getPServiceId());
+
         return serviceDAO.find(possibleService.getServiceId());
     }
 
     @Override
     public List<PossibleService> findAllPossibleServicesBySuggestionId(Number suggestionId) {
+        log.debug("Getting all possible services by suggestion id {}", suggestionId);
+
         List<PossibleService> possibleServices = new ArrayList<>();
 
         possibleServices.addAll(getJdbcTemplate().query(FIND_ALL_P_SERVICES_BY_SUGGESTION_ID,
@@ -93,6 +97,10 @@ public class PossibleServiceDAOImpl extends CrudDAOImpl<PossibleService> impleme
 
     @Override
     public void buyService(Ticket ticket, PossibleService possibleService) {
+        log.debug("Buying possible service (id = {}) by ticket (id = {})",
+                  possibleService.getPServiceId(),
+                  ticket.getTicketId());
+
         getJdbcTemplate().update(BUY_P_SERVICE, possibleService.getPServiceId(), ticket.getTicketId());
     }
 }
