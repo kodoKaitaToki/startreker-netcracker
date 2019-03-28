4657d637bc385c50c9b956a2e13100f653a92858
AlexXDD <OlehI.Demydenko@gmail.com>
2019-03-28 02:45:36 +0200

    K19DKPA-257: Added discount expire check

--- frontend/src/main/web/src/app/pages/flights/shared/services/discount-calc.helper.ts
+++ frontend/src/main/web/src/app/pages/flights/shared/services/discount-calc.helper.ts
@@ -1,7 +1,7 @@
 import {TicketClass} from "../../../carrier/discounts/shared/model/ticket-class.model";
 
 export function calcPriceWithDiscount(ticketClass: TicketClass): number {
-  if (ticketClass.discount !== undefined) {
+  if (ticketClass.discount !== undefined && !ticketClass.discount.is_expired) {
     let price: number = ticketClass.ticket_price;
     let rate: number = ticketClass.discount.discount_rate;
 
